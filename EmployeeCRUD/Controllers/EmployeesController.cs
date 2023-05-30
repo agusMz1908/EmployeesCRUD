9437ac2c56ebdc292123baf79d3819062c6b501f
Agustin Muniz <agus.muniz1997@gmail.com>
2023-05-30 01:16:25 -0300

    Add mail validation in add and update, to avoid duplicate data

--- EmployeeCRUD/Controllers/EmployeesController.cs
+++ EmployeeCRUD/Controllers/EmployeesController.cs
@@ -2,23 +2,25 @@
 using EmployeeCRUD.Models;
 using EmployeeCRUD.Models.Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
+
 
 namespace EmployeeCRUD.Controllers
 {
     public class EmployeesController : Controller
     {
-        private readonly ApplicationDbContext applicationDbContext;
+        private readonly ApplicationDbContext _applicationDbContext;
 
         public EmployeesController(ApplicationDbContext applicationDbContext)
         {
-            this.applicationDbContext = applicationDbContext;
+            _applicationDbContext = applicationDbContext;
         }
 
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var employees = await applicationDbContext.Employees.ToListAsync();
+            var employees = await _applicationDbContext.Employees.ToListAsync();
             return View(employees);
             
         }
@@ -33,43 +35,57 @@ namespace EmployeeCRUD.Controllers
         public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest) 
         {
             if (ModelState.IsValid) 
-            { 
-                var employee = new Employee()
+            {
+                if (await _applicationDbContext.Employees.AnyAsync(e => e.Email == addEmployeeRequest.Email)) 
                 {
-                    Id = Guid.NewGuid(),
-                    Name = addEmployeeRequest.Name,
-                    Email = addEmployeeRequest.Email,
-                    Salary = addEmployeeRequest.Salary,
-                    DateOfBirth = addEmployeeRequest.DateOfBirth,
-                    Department = addEmployeeRequest.Department
-                };
+                    ModelState.AddModelError("Email", "This employee alredy exist");
+                    return View(addEmployeeRequest);
+                }
 
-                await applicationDbContext.Employees.AddAsync(employee);
-                await applicationDbContext.SaveChangesAsync();
-                return RedirectToAction("Index");
-            }
+                if (ModelState.IsValid) 
+                { 
+                    var employee = new Employee()
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = addEmployeeRequest.Name,
+                        Email = addEmployeeRequest.Email,
+                        Salary = addEmployeeRequest.Salary,
+                        DateOfBirth = addEmployeeRequest.DateOfBirth,
+                        Department = addEmployeeRequest.Department
+                    };
 
+                    await _applicationDbContext.Employees.AddAsync(employee);
+                    await _applicationDbContext.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+
+
+                }
             return View(addEmployeeRequest);
         }
 
         [HttpGet]
-        public async Task<IActionResult> View(Guid id) 
+        public async Task<IActionResult> View(Guid id)
         {
-            var employee = await applicationDbContext.Employees.FirstOrDefaultAsync(e => e.Id == id);
+            var employee = await _applicationDbContext.Employees.FirstOrDefaultAsync(e => e.Id == id);
 
-            if (employee != null) 
+            if (employee != null)
             {
-                var viewModel = new UpdateEmployeeViewModel()
-                {
-                    Id = employee.Id,
-                    Name = employee.Name,
-                    Email = employee.Email,
-                    Salary = employee.Salary,
-                    DateOfBirth = employee.DateOfBirth,
-                    Department = employee.Department
-                };
+                var viewModel = new UpdateEmployeeViewModel(_applicationDbContext);
 
-                return await Task.Run(() => View("View", viewModel));
+                viewModel.Id = employee.Id;
+                viewModel.Name = employee.Name;
+                viewModel.Email = employee.Email;
+                viewModel.Salary = employee.Salary;
+                viewModel.DateOfBirth = employee.DateOfBirth;
+                viewModel.Department = employee.Department;
+
+                if (!await viewModel.IsEmailUnique(viewModel.Id, viewModel.Email))
+                {
+                    ModelState.AddModelError("Email", "This email already exists.");
+                }
+
+                return View("View", viewModel);
             }
 
             return RedirectToAction("Index");
@@ -78,33 +94,49 @@ namespace EmployeeCRUD.Controllers
         [HttpPost]
         public async Task<IActionResult> View(UpdateEmployeeViewModel model)
         {
-            var employee = await applicationDbContext.Employees.FindAsync(model.Id);
-
-            if (employee != null)
+            if (ModelState.IsValid)
             {
-                employee.Name = model.Name;
-                employee.Email = model.Email;
-                employee.Salary = model.Salary;
-                employee.DateOfBirth = model.DateOfBirth;
-                employee.Department = model.Department;
+                var employee = await _applicationDbContext.Employees.FindAsync(model.Id);
 
-                await applicationDbContext.SaveChangesAsync();
+                if (employee != null)
+                {
+                    if (await IsEmailUnique(model.Id, model.Email))
+                    {
+                        employee.Name = model.Name;
+                        employee.Email = model.Email;
+                        employee.Salary = model.Salary;
+                        employee.DateOfBirth = model.DateOfBirth;
+                        employee.Department = model.Department;
 
-                return RedirectToAction("Index");
+                        await _applicationDbContext.SaveChangesAsync();
+
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("Email", "This email already exists.");
+                    }
+                }
             }
 
-            return RedirectToAction("Index");
+            return await Task.Run(() => View("View", model));
+        }
+
+        private async Task<bool> IsEmailUnique(Guid id, string email)
+        {
+            var employee = await _applicationDbContext.Employees.FirstOrDefaultAsync(e => e.Email == email && e.Id != id);
+            return employee == null;
         }
 
         [HttpPost]
         public async Task<IActionResult> Delete(UpdateEmployeeViewModel model)
         {
-            var employee = await applicationDbContext.Employees.FindAsync(model.Id);
+            var employee = await _applicationDbContext.Employees.FindAsync(model.Id);
 
             if (employee != null)
             {
-                applicationDbContext.Employees.Remove(employee);
-                await applicationDbContext.SaveChangesAsync();
+                _applicationDbContext.Employees.Remove(employee);
+                await _applicationDbContext.SaveChangesAsync();
 
                 return RedirectToAction("Index");
             }
@@ -113,3 +145,5 @@ namespace EmployeeCRUD.Controllers
         }
     }
 }
+
+
--- EmployeeCRUD/Models/UpdateEmployeeViewModel.cs
+++ EmployeeCRUD/Models/UpdateEmployeeViewModel.cs
@@ -1,19 +1,41 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using EmployeeCRUD.Models.Domain;
+using EmployeeCRUD.Data;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeCRUD.Models
 {
     public class UpdateEmployeeViewModel
     {
+        private ApplicationDbContext applicationDbContext;
+        public UpdateEmployeeViewModel()
+        {
+            // Constructor sin parámetros necesario para el enlace de modelo
+        }
+
+        public UpdateEmployeeViewModel(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
         public Guid Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "The Name is Required")]
         public string Name { get; set; }
-        [Required]
+        [Required(ErrorMessage = "The Email is Required")]
         public string Email { get; set; }
-        [Required]
+        [DefaultValue(1)]
+        [Range(25000, long.MaxValue, ErrorMessage = "Salary must be greater than 25.000")]
         public long Salary { get; set; }
-        [Required]
+        [Required(ErrorMessage = "The date is required")]
         public DateTime DateOfBirth { get; set; }
-        [Required]
+        [Required(ErrorMessage = "The department is Required")]
         public string Department { get; set; }
+
+        public async Task<bool> IsEmailUnique(Guid id, string email)
+        {
+            var existingEmployee = await applicationDbContext.Employees.FirstOrDefaultAsync(e => e.Email == email && e.Id != id);
+            return existingEmployee == null;
+        }
     }
 }
